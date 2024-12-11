ce92c116142b2579fff083628f27382c94e93138
Sung Yeob Jung <dev.sungyeob@gmail.com>
2024-12-11 17:14:16 +0900

    update all

--- src/test/java/com/hana4/springexam2/controller/CommentControllerTest.java
+++ src/test/java/com/hana4/springexam2/controller/CommentControllerTest.java
@@ -1,4 +1,116 @@
 package com.hana4.springexam2.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hana4.springexam2.entity.Comment;
+import com.hana4.springexam2.entity.Post;
+import com.hana4.springexam2.entity.User;
+import com.hana4.springexam2.repository.CommentRepository;
+import com.hana4.springexam2.repository.PostRepository;
+import com.hana4.springexam2.repository.UserRepository;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class CommentControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@Autowired
+	private CommentRepository commentRepository;
+
+	@Autowired
+	private PostRepository postRepository;
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@BeforeEach
+	public void setup() {
+		User writer = new User("testUser", "test@example.com");
+		userRepository.save(writer);
+
+		Post post = new Post("Initial Title", writer, "Initial Body");
+		postRepository.save(post);
+
+		for (short n = 1; n <= 5; n++) {
+			commentRepository.save(new Comment("Initial Comment" + n, post, writer));
+		}
+	}
+
+	@Test
+	@Order(1)
+	void createComment() throws Exception {
+		User writer = userRepository.findAll().get(0);
+		Post post = postRepository.findAll().get(0);
+
+		Map<String, String> request = new HashMap<>();
+		request.put("body", "Sample comment title");
+		request.put("post", post.getId().toString());
+		request.put("writer", writer.getId());
+		mockMvc.perform(post("/comments/{id}", post.getId()).contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(request)))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.body").value("Sample comment title"))
+			.andExpect(jsonPath("$.post").value(post.getId().toString()))
+			.andExpect(jsonPath("$.writer").value(writer.getId()));
+	}
+
+	@Test
+	@Order(2)
+	void getComments() throws Exception {
+		Post post = postRepository.findAll().get(0);
+		mockMvc.perform(get("/comments/{id}", post.getId()))
+			.andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$").isArray())
+			.andExpect(jsonPath("$.length()").value(5));
+	}
+
+	@Test
+	@Order(3)
+	void updateComment() throws Exception {
+		Comment comment = commentRepository.findAll().get(0);
+
+		Map<String, String> request = new HashMap<>();
+		request.put("body", "Updated comment title");
+
+		mockMvc.perform(patch("/comments/{id}", comment.getId()).contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(request)))
+			.andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.body").value("Updated comment title"))
+			.andExpect(jsonPath("$.post").value(comment.getPost().getId().toString()))
+			.andExpect(jsonPath("$.writer").value(comment.getWriter().getId()));
+	}
+
+	@Test
+	@Order(4)
+	void deleteComment() throws Exception {
+		mockMvc.perform(delete("/comments/{id}", commentRepository.findAll().get(0).getId()))
+			.andExpect(status().isOk());
+	}
 }
--- src/test/java/com/hana4/springexam2/controller/PostControllerTest.java
+++ src/test/java/com/hana4/springexam2/controller/PostControllerTest.java
@@ -1,13 +1,17 @@
 package com.hana4.springexam2.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -26,7 +30,7 @@ import jakarta.persistence.EntityManager;
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
-// @TestMethodOrder(MethodOrderer.OrderAnnotation.class)//실행 순서 지정 가능
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)//실행 순서 지정 가능
 public class PostControllerTest {
 
 	@Autowired
@@ -49,11 +53,24 @@ public class PostControllerTest {
 		User writer = new User("testUser", "test@example.com");
 		userRepository.save(writer);
 
-		Post post = new Post("Initial Title", writer, "Initial Body");
-		postRepository.save(post);
+		for (short n = 1; n <= 5; n++) {
+			postRepository.save(new Post("Initial Title" + n, writer, "Initial Body" + n));
+		}
+
 	}
 
 	@Test
+	@Order(2)
+	void getAllPosts() throws Exception {
+		mockMvc.perform(get("/post").contentType(MediaType.APPLICATION_JSON))
+			.andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$").isArray())
+			.andExpect(jsonPath("$.length()").value(5));
+	}
+
+	@Test
+	@Order(1)
 	void createPostTest() throws Exception {
 		User writer = userRepository.findAll().get(0);
 
@@ -67,20 +84,41 @@ public class PostControllerTest {
 			.andExpect(status().isCreated())
 			.andExpect(jsonPath("$.title").value("Sample Post Title"))
 			.andExpect(jsonPath("$.body").value("Sample Post Body"))
-			.andExpect(jsonPath("$.writer").value(writer.getId()));
+			.andExpect(jsonPath("$.writer").value(writer.getId())).andDo(print());
 	}
 
 	@Test
+	@Order(4)
 	void updatePostTest() throws Exception {
-		User writer = userRepository.findAll().get(0);
 		Map<String, String> request = new HashMap<>();
+		System.out.println("hi");
 		request.put("title", "Update Post Title");
 		request.put("body", "Update Post Body");
-		mockMvc.perform(patch("/post/{id}", 1)
+		mockMvc.perform(patch("/post/{id}", postRepository.findAll().get(0).getId())
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(objectMapper.writeValueAsString(request)))
-			.andExpect(status().isOk());
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.title").value("Update Post Title"))
+			.andExpect(jsonPath("$.body").value("Update Post Body"));
 	}
+
+	@Test
+	@Order(3)
+	void deletePostTest() throws Exception {
+		// postRepository.findAll().get(0)
+		mockMvc.perform(delete("/post/{id}", postRepository.findAll().get(0).getId())
+		).andExpect(status().isOk());
+	}
+
+	@Test
+	@Order(5)
+	void getPostByIdTest() throws Exception {
+		mockMvc.perform(get("/post/{id}", postRepository.findAll().get(0).getId()))
+			.andDo(print())
+			.andExpect(status().isOk()).andExpect(jsonPath("$.title").value("Initial Title1"))
+			.andExpect(jsonPath("$.body").value("Initial Body1"));
+	}
+
 }
 
 // import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
